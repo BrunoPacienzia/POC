f123bc7c723054b43cddc461a08d41e020fb53df
Lucas Pennice <lucaspennice@gmail.com>
2024-09-26 17:39:33 -0300

    test generados y serie vs paralelo

--- test/index.test.js
+++ test/index.test.js
@@ -1,46 +1,124 @@
-import assert from "assert";
-import { asincrona, sincrona, fetchData, fetchData_2 } from "../index.js";
+import assert from 'assert';
+import { asincrona, sincrona, fetchData, fetchData_2 } from '../index.js';
 
 // Testeamos funcion sincrona
 
-describe.only("sincrona", function () {
-  describe("#indexOf()", function () {
-    it("should return -1 when the value is not present", function () {
-      debugger;
-      assert.equal(sincrona.i(), -1);
-    });
-  });
+// describe('sincrona', function () {
+// 	describe('#indexOf()', function () {
+// 		it('should return -1 when the value is not present', function () {
+// 			debugger;
+// 			assert.equal(sincrona.i(), -1);
+// 		});
+// 	});
+// });
+
+// describe.skip('sincrona', function () {
+// 	it('should return data from the promise', function () {
+// 		return fetchData().then((data) => {
+// 			assert.equal(data, 'Data retrieved successfully!'); // Verificamos que los datos son correctos
+// 		});
+// 	});
+// 	it('should retrieve data successfully', async function () {
+// 		const data = await fetchData_2(); // Esperamos a que la promesa se resuelva
+// 		assert.equal(data, 'Data retrieved successfully!'); // Verificamos el valor devuelto
+// 	});
+// });
+
+// // Testeamos async/await con Promesas
+
+// describe.skip('asincrona', function () {
+// 	describe('#delayedPromise()', function () {
+// 		it('should return 5 after 100ms', async function () {
+// 			const resultado = await asincrona.delayedPromise();
+// 			assert.equal(resultado, 5);
+// 		});
+// 	});
+// });
+
+// describe.skip('asincrona', function () {
+// 	describe('#delayedPromise()', function () {
+// 		it('should return 5 after 1000ms', function () {
+// 			return asincrona.delayedPromise().then((resolve) => {
+// 				assert.equal(resolve, 5);
+// 			});
+// 		});
+// 	});
+// });
+
+function add(args) {
+	return args.reduce((prev, curr) => prev + curr, 0);
+}
+
+describe.only('add() en paralelo 2', function () {
+	let startTime;
+
+	before(function () {
+		startTime = Date.now();
+	});
+
+	after(function () {
+		const endTime = Date.now();
+		const duration = endTime - startTime;
+		console.log(`Test suite took ${duration}ms to run.`);
+	});
+
+	let tests = [];
+
+	for (let i = 0; i < 7000; i++) {
+		let newElement = { args: [], expected: sumToN(i + 1) };
+
+		for (let j = 0; j < i + 1; j++) {
+			newElement.args.push(j + 1);
+		}
+
+		tests.push(newElement);
+	}
+
+	tests.forEach(({ args, expected }) => {
+		it(`correctly adds ${args.length} args`, function () {
+			const res = add(args);
+			assert.strictEqual(res, expected);
+		});
+	});
 });
 
-describe.skip("sincrona", function () {
-  it("should return data from the promise", function () {
-    return fetchData().then((data) => {
-      assert.equal(data, "Data retrieved successfully!"); // Verificamos que los datos son correctos
-    });
-  });
-  it("should retrieve data successfully", async function () {
-    const data = await fetchData_2(); // Esperamos a que la promesa se resuelva
-    assert.equal(data, "Data retrieved successfully!"); // Verificamos el valor devuelto
-  });
-});
+function sumToN(n) {
+	let sum = 0;
+	for (let i = 0; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
+}
 
-// Testeamos async/await con Promesas
+describe('add()', function () {
+	let startTime;
 
-describe.skip("asincrona", function () {
-  describe("#delayedPromise()", function () {
-    it("should return 5 after 100ms", async function () {
-      const resultado = await asincrona.delayedPromise();
-      assert.equal(resultado, 5);
-    });
-  });
-});
+	before(function () {
+		startTime = Date.now();
+	});
 
-describe.skip("asincrona", function () {
-  describe("#delayedPromise()", function () {
-    it("should return 5 after 1000ms", function () {
-      return asincrona.delayedPromise().then((resolve) => {
-        assert.equal(resolve, 5);
-      });
-    });
-  });
+	after(function () {
+		const endTime = Date.now();
+		const duration = endTime - startTime;
+		console.log(`Test suite took ${duration}ms to run.`);
+	});
+
+	let tests = [];
+
+	for (let i = 0; i < 7000; i++) {
+		let newElement = { args: [], expected: sumToN(i + 1) };
+
+		for (let j = 0; j < i + 1; j++) {
+			newElement.args.push(j + 1);
+		}
+
+		tests.push(newElement);
+	}
+
+	tests.forEach(({ args, expected }) => {
+		it(`correctly adds ${args.length} args`, function () {
+			const res = add(args);
+			assert.strictEqual(res, expected);
+		});
+	});
 });
